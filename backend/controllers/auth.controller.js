c5136f336149a2d6954664380c719e6d07bd4b1e
Luis <solarluiso@gmail.com>
2025-01-03 21:36:37 -0500

    implement some signup functionality with user validation; update routes to use POST method and enable JSON parsing in server

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,26 @@
-export const signup = (req, res) => {
-  res.send("Signup route");
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      role: user.role,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = (req, res) => {
