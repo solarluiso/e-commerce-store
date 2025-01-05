0e973fb4023870f098ce07b1dc87ba31f868f1af
Luis <solarluiso@gmail.com>
2025-01-05 18:29:35 -0500

    add JWT authentication with access and refresh tokens; implement Redis for token storage and set cookies in response

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,43 @@
+import jwt from "jsonwebtoken";
+
 import User from "../models/user.model.js";
+import { redis } from "../lib/redis.js";
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60 // 7 days
+  );
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // prevent XSS attacks, cross site scripting attack
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, // prevent XSS attacks, cross site scripting attack
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -10,11 +49,16 @@ export const signup = async (req, res) => {
     }
     const user = await User.create({ name, email, password });
 
+    // authenticate
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      password: user.password,
       role: user.role,
     });
   } catch (error) {
