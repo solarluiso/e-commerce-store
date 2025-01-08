ea45fb516f69ae863b0f2fc310fb695d36bd08fa
Luis <solarluiso@gmail.com>
2025-01-08 07:55:47 -0500

    add getFeaturedProducts controller and route to fetch featured products with caching in Redis

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import { redis } from "../lib/redis.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -6,6 +7,28 @@ export const getAllProducts = async (req, res) => {
     res.json({ products }); // Send products as response
   } catch (error) {
     console.log("Error in getAllProducts controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products"); // Fetch featured products from redis
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+    // if not in redis, fetch from mongodb
+    featuredProducts = await Product.find({ isFeatured: true }).lean(); // Fetch featured products from the database (and convert to plain JS objects)
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    await redis.set("featured_products", JSON.stringify(featuredProducts)); // Cache featured products in redis for future quick requests
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller", error.message);
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
