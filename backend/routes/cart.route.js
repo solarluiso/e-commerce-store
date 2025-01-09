160c470aa30219d2e0d59f08d9765c0e933661ad
Luis <solarluiso@gmail.com>
2025-01-09 18:37:00 -0500

    add updateQuantity controller and route to handle updating product quantity in cart

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -3,6 +3,7 @@ import express from "express";
 import {
   addToCart,
   removeAllFromCart,
+  updateQuantity,
 } from "../controllers/cart.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.get("/", protectRoute, addToCart);
 router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
 
 export default router;
