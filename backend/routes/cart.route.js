56f3c62b13fbb344f498466b7427001a739c4fc5
Luis <solarluiso@gmail.com>
2025-01-09 18:15:59 -0500

    add addToCart controller and route to handle adding products to the user's cart

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { addToCart } from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, addToCart);
+
+export default router;
