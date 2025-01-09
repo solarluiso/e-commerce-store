c9620077decc50081abdd47a70e2f38b84e96178
Luis <solarluiso@gmail.com>
2025-01-09 18:38:35 -0500

    fix cart route method from GET to POST for adding items to the cart

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -9,7 +9,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, addToCart);
+router.post("/", protectRoute, addToCart);
 router.delete("/", protectRoute, removeAllFromCart);
 router.put("/:id", protectRoute, updateQuantity);
 
