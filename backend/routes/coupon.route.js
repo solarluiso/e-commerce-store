68d221318ca4a2b199755c15739428b533e317d7
Luis <solarluiso@gmail.com>
2025-01-10 20:54:03 -0500

    add validateCoupon controller and route to check coupon validity and expiration

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/auth.middleware";
-import { getCoupon } from "../controllers/coupon.controller";
+import { getCoupon, validateCoupon } from "../controllers/coupon.controller";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getCoupon);
+router.post("/validate", protectRoute, validateCoupon);
 
 export default router;
