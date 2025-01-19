0b0d4174c1bb7d46358998280e03e31045ea3bf2
Luis <solarluiso@gmail.com>
2025-01-18 20:26:59 -0500

    add Navbar component and implement routing for SignUp and Login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
