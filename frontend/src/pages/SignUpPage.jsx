d97f9eec36576d7881674f33db15896dc29babe8
Luis <solarluiso@gmail.com>
2025-01-22 10:48:53 -0500

    implement user signup functionality with Zustand store and integrate it to SignUpPage

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -11,11 +12,11 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
-  const loading = false;
+  const { signup, loading } = useUserStore();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    signup(formData);
   };
 
   return (
