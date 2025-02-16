3cce34f6ca068e3313d5d1f74e9588567ad29f23
Luis <solarluiso@gmail.com>
2025-02-16 08:41:22 -0500

    add cart functionality to Navbar and ProductCard components

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,15 +2,18 @@ import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
 
 import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const ProductCard = () => {
   const { user } = useUserStore();
+  const { addToCart } = useCartStore();
   const handleAddToCart = () => {
     if (!user) {
       toast.error("Please login to add products to cart", { id: "login" });
       return;
     } else {
       // add to cart
+      addToCart(product);
     }
   };
 
