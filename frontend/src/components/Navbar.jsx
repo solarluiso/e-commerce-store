7c98fc9f462fdccc1b01d191d8fde0c2f6d17d7c
Luis <solarluiso@gmail.com>
2025-07-21 17:25:59 -0400

    remove Home link from Navbar for streamlined navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,12 +19,6 @@ const Navbar = () => {
             Trendspace
           </Link>
           <nav className="flex flex-wrap items-center gap-4">
-            <Link
-              to="/"
-              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
-            >
-              Home
-            </Link>
             {user && (
               <Link
                 to={"/cart"}
