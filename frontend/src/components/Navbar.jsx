0fc403af182d29fcb36463e5c6c7ff3497c3acd6
Luis <solarluiso@gmail.com>
2025-07-22 11:21:24 -0400

    update Navbar component styling for admin link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
             {isAdmin && (
               <Link
                 to={"/secret-dashboard"}
-                className="bg-emerald-700 hover:bg-emerald-600 text-white font-medium px-3 py-1 rounded-md transition duration-300 ease-in-out flex items-center"
+                className="bg-emerald-700 hover:bg-emerald-600 text-white font-medium py-2 px-4 rounded-md transition duration-300 ease-in-out flex items-center"
               >
                 <Lock className="inline-block mr-1" size={18} />
                 <span className="hidden sm:inline">Dashboard</span>
